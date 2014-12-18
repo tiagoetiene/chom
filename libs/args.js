f54c5fcf06a91863fb9ccc7a68ffb26820c296a9
Tiago Etiene <tiago.etiene@gmail.com>
2014-12-17 23:09:25 -0800

    Added --predicate parameter

--- libs/args.js
+++ libs/args.js
@@ -7,6 +7,7 @@ program
 	.option( '--stream', 'Stream the latest tweets' )
 	.option( '--field [key]', 'Field to be used during summarization' )
 	.option( '--depth', 'Max tree depth', 9 )
+	.option( '--predicate [pred]', 'Boolean function that says whether a tweet should be discarde (false) or not (true) ', "return true;" )
 	.option( '-m, --min [tweet_field]', 'Find the minimum value of a set' )
 	.option( '-M, --max [tweet_field]', 'Find the maximum value of a set ' )
 	.option( '-c, --config [type]', 'Config file' )
@@ -39,4 +40,8 @@ if( program.field ) {
 
 if( program.depth ) {
 	params[ "depth" ] = program.depth;
+}
+
+if( program.predicate ) {
+	params[ "predicate" ] = program.predicate;
 }
