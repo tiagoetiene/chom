37988b9c8c68366b27ecf6c64675a78b33653965
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-12-21 09:16:42 -0800

    Unnecessary parseList was removed

--- filter/args.js
+++ filter/args.js
@@ -1,9 +1,5 @@
 program = require('commander');
 
-function parserList( list ) {
-	return list.split(",");
-}
-
 program
 	.version( '0.0.1' )
 	.option( '-p --predicate [pred]', 'boolean evaluated function deciding whether a tweet should be discarde (false) or not (true) ', "return true;" )
