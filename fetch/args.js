1b60766399e8513265b931b21773c4864033b6dd
Tiago Etiene <tiago.etiene@gmail.com>
2014-12-23 16:36:08 -0800

    Added option to close the database when its end is reached

--- fetch/args.js
+++ fetch/args.js
@@ -5,6 +5,7 @@ program
 	.option( '-c, --config [type]', 'configuration file that may include one or more command show in this menu' )
 	.option( '-t --stream', 'stream the latest tweets from Twitter servers' )
 	.option( '-e --search [tweetID]', 'Search for past tweets', 0 )
+	.option( '--close', 'close database on exist', false )
 	.parse( process.argv );
 
 if( process.argv.length <= 2 ) {
