5efb3ee042200eeb0d47e6eeb9eea23f5305a56f
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-01-02 17:54:39 -0800

    Added cast to mongodb ObjectID

--- save/args.js
+++ save/args.js
@@ -10,7 +10,7 @@ program
 	.option( '-k, --keys [list]', "comma separated list of keys to be used during summarization", parserList )
 	.option( '-c, --config [type]', 'configuration file that may include one or more command show in this menu' )
 	.option( '-m, --max [number]', 'maximum number of objects to be kept in memory', 50000, parseInt )
-	.option( '--cast [list]', 'comma separated list of pairs "key,type", where key is converted to type "type" ', parserList )
+	.option( '--cast [list]', 'comma separated list of pairs "key,type", where key is converted to type "type". type is one of: date, objectid ', parserList )
 	.option( '--code [code]', 'code to be run at input json object "datum"' )
 	.option( '--save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
 	.option( '--collection [name]', 'name of the collection to be saved to' )
