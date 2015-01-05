eff832bda680f738e922d41b586a99715be5642d
Tiago Etiene <tiago.etiene@gmail.com>
2015-01-04 17:27:16 -0800

    Adding shortcuts

--- fetch/args.js
+++ fetch/args.js
@@ -6,9 +6,9 @@ program
 	.option( '-o, --collection [name]', 'name of the collection to read from' )
 	.option( '-c, --config [type]', 'configuration file that may include one or more command show in this menu' )
 	.option( '-g, --group [group]', 'group to be used', 0, parseInt )
-	.option( '-t --stream', 'stream the latest tweets from Twitter servers' )
-	.option( '-e --search [tweetID]', 'Search for past tweets', 0 )
-	.option( '--close', 'close database on exist', false )
+	.option( '-t, --stream', 'stream the latest tweets from Twitter servers' )
+	.option( '-e, --search [tweetID]', 'Search for past tweets', 0 )
+	.option( '-l, --close', 'close database on exit', false )
 	.parse( process.argv );
 
 if( process.argv.length <= 2 ) {
