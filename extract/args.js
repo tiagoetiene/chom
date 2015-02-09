031d5361f6bd5c3af251a2bc26d2c2aae09a0edd
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-02-08 21:25:14 -0800

    Added option to create an object

--- extract/args.js
+++ extract/args.js
@@ -3,6 +3,7 @@ program = require('commander');
 
 program
 	.version( '0.0.1' )
+	.option( '-c, --create-object', 'creates a json object using the input fields' )
 	.option( '-f, --fields [field]', "field which values will be extracted", utils.parserList, [] )
 	.option( '-d, --delimiter [dem]', "delimiter between extracted value" )
 	.parse( process.argv );
