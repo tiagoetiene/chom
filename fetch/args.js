487625000c3a62cfb4a38a36d278781ca03e67a8
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-02-13 14:15:42 -0800

    Added option to define language

--- fetch/args.js
+++ fetch/args.js
@@ -4,13 +4,14 @@ program = require('commander');
 
 program
 	.version( '0.0.1' )
-	.option( '-l, --close', 'close database on exit', false )
+	.option( '-C, --close', 'close database on exit', false )
 	.option( '-o, --collection [name]', 'name of the collection to read from' )
 	.option( '-r, --credentials [json]', 'json file containing the twitter credentials' )
 	.option( '-c, --config [type]', 'configuration file that may include one or more command show in this menu' )
 	.option( '-f, --first [number]', 'retrieve the first [number] items', parseInt )
 	.option( '-g, --group [group]', 'group to be used', 0, parseInt )
 	.option( '-k, --keywords [list]', 'comma separated list of keywords', utils.parserList, [] )
+	.option( '-l, --lang [language]', 'language to be used (only works for twitter requests)', 'en' )
 	.option( '-t, --stream', 'stream the latest tweets from Twitter servers' )
 	.option( '-e, --search [tweetID]', 'Search for past tweets', 0 )
 	.option( '-t, --twitter-screen-names [user_1,user_2,...]', 'Retrieve the user screen names', utils.parserList, []  )
