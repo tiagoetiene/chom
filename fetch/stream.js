65b0a483588a73b2648ad4a05329a64b16a8c2ed
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-07-28 08:45:10 -0700

    Refactoring fetch

--- fetch/stream.js
+++ fetch/stream.js
@@ -1,37 +1,60 @@
 var _ 		= require( "underscore" );
 var utils = require('../libs/utils');
+var dns		= require('dns');
+
+var streaming;
+
 
 module.exports = {
 	start : function( keywords, lang, credentials, callback ) {
+		setInterval( function atEachInterval() {
 
-		//
-		// If no credentials were passed as input,
-		// then lookup the database
-		//
-		if( _.isUndefined( credentials ) ) {
-			credentials = utils.getCredential();
-		}
+			if( streaming === true ) {
+				return;
+			}
 
-		//
-		// If no credentials were found, then return
-		//
-		if( _.isEmpty( credentials ) ) {
-			return;
-		}
+			dns.resolve( 'www.google.com', function(err) {
 
-		//
-		// Connecting to twitter
-		//
-		var T = new Twit( credentials );
+				if ( err ) {
+					streaming = false;
+					return;
+				}
 
-		//
-		// Starting stream of tweets
-		//
-		var stream = T.stream( "statuses/filter", { track: keywords, language: lang });
+				streaming = true;
+				
+				//
+				// If no credentials were passed as input,
+				// then lookup the database
+				//
+				if( _.isUndefined( credentials ) ) {
+					credentials = utils.getCredential();
+				}
 
-		//
-		// Starting stream
-		//
-		stream.on( "tweet", callback );		
+				//
+				// If no credentials were found, then return
+				//
+				if( _.isEmpty( credentials ) ) {
+					return;
+				}
+
+				//
+				// Connecting to twitter
+				//
+				var T = new Twit( credentials );
+
+				//
+				// Starting stream of tweets
+				//
+				var stream = T.stream( "statuses/filter", { track: keywords, language: lang });
+
+				//
+				// Starting stream
+				//
+				stream.on( "tweet", callback );
+				stream.on( "error", function( err ) { stream.stop(); } );
+
+
+			} );
+		}, 1000 );
 	}
 }
