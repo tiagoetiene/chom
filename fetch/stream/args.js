d61a3bbf84e680a50384a619c2e097541cc9fd92
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-08-02 14:46:56 -0700

    Added no-urls options

--- fetch/stream/args.js
+++ fetch/stream/args.js
@@ -7,6 +7,7 @@ program
 	.option( '-k, --keywords [list]', 'comma separated list of keywords', utils.parserList, [] )
 	.option( '-l, --lang [language]', 'language to be used (only works for twitter requests)', 'en' )
 	.option( '-n, --no-retweets', 'no retweets' )
+	.option( '-u, --no-url', 'no urls' )
 	.parse( process.argv );
 
 if( process.argv.length <= 3 ) {
