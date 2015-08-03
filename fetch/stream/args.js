71e4d9930f1023be236b6ef4cbb15ac6ec55e62e
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-08-03 08:54:20 -0700

    Added option to track only geo-located tweets

--- fetch/stream/args.js
+++ fetch/stream/args.js
@@ -8,6 +8,7 @@ program
 	.option( '-l, --lang [language]', 'language to be used (only works for twitter requests)', 'en' )
 	.option( '-r, --no-retweet', 'no retweets' )
 	.option( '-u, --no-url', 'no urls' )
+	.option( '-g, --geo', 'only geolocated tweets' )
 	.parse( process.argv );
 
 if( process.argv.length <= 3 ) {
