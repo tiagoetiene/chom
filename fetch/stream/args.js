688b8c113b379e956ec5db52cb9bcffd9cc852a0
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-08-05 15:28:10 -0700

    Added -c, --city option to chom-fetch-stream
    Added possibility of setting the location from which one wants to
    retrieve tweets

--- fetch/stream/args.js
+++ fetch/stream/args.js
@@ -4,6 +4,7 @@ program = require('commander');
 
 program
 	.version( '0.0.1' )
+	.option( '-c, --city [list]', 'comma separated list of keywords', utils.parserList, [] )
 	.option( '-k, --keywords [list]', 'comma separated list of keywords', utils.parserList, [] )
 	.option( '-l, --lang [language]', 'language to be used (only works for twitter requests)', 'en' )
 	.option( '-r, --no-retweet', 'no retweets' )
