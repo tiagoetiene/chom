c174dcb7f2c412cad1253b4ddbf2dc6813fccb50
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2015-11-15 15:07:21 -0800

    Add #push to set

--- database/set/args.js
+++ database/set/args.js
@@ -9,6 +9,7 @@ program
 	.option( '-k, --key [name]', 'key to be used to identify documents' )
 	.option( '-s, --save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
 	.option( '-i, --simulate', 'do not save to database, only simulate', false, false )
+	.option( '-p, --push [name]', 'name of the collection to be saved to' )
 	.parse( process.argv );
 
 if( process.argv.length <= 2 ) {
