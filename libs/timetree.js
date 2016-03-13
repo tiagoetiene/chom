62103f26a18f826d507b97521db18592c6edf6a6
Tiago Etiene <tiago.etiene@gmail.com>
2016-03-12 19:03:51 -0800

    Simplify code

--- libs/timetree.js
+++ libs/timetree.js
@@ -1,151 +1,81 @@
+"use strict";
+var _ = require( "underscore" );
+
+class TweetHistogram {
+	constructor( binSize, valuer ) {
+		this.binSize = binSize
+		this.valuer = valuer
+		this.hist = new Map()
+		this.data = new Map()
+		this.updt = new Map()
+  	}
+
+  	getBin( date ) {
+  		return Math.floor( date / this.binSize ) * this.binSize
+  	}
+
+  	add( tweet ) {
+  		var date = +( new Date( tweet.created_at ) )
+  		var bin = this.getBin( date )
+  		var val = this.valuer.call( this, tweet )
+  		if( this.hist.has( bin ) == false ) {
+  			this.hist.set( bin, 0 )
+  			this.data.set( bin, 0 )
+  		}
+  		this.hist.set( bin, this.hist.get( bin ) + 1 )
+  		this.data.set( bin, this.data.get( bin ) + val )
+  		this.updt.set( bin, true )
+  	}
+
+  	updated( date ) {
+  		this.updt.set( this.getBin( +date ), false )
+  	}
+
+  	values() {
+  		var data = []
+  		for( let keyPair of this.hist ) {
+  			var bin = keyPair[ 0 ]
+  			var tweetCount = keyPair[ 1 ]
+  		  	data.push( { bin : bin, 
+  		  				 count : tweetCount,
+  		  				 value : this.data.get( bin ),
+  		  				 updated : this.updt.get( bin ) } )
+		}
+  		return _.sortBy( data, 'bin');
+  	}
+}
+
+class TimeTree {
+	constructor( depth, valuer ) {
+		var milliseconds = 1000.0
+		this.depth = depth
+		this.histograms = new Map()
+		for( var i = 0; i < depth; ++i ) {
+			var hist = new TweetHistogram( milliseconds, valuer )
+			this.histograms.set( i, hist )
+			milliseconds *= 4;
+		}
+	}
+
+	add( tweet ) {
+		for( let keyPair of this.histograms ) {
+			var depth = keyPair[ 0 ]
+			var hist = keyPair[ 1 ]
+			hist.add( tweet )
+		}
+	}
+
+	updated( depth, date ) {
+		this.histograms.get( depth ).updated( date )
+	}
+
+	values( depth ) {
+		return this.histograms.get( depth ).values()
+	}
+}
+
 module.exports = {
-	TimeTree : function( ) {
-		var Class = require('jsclass/src/core').Class;
-		var Comparable = require('jsclass/src/comparable').Comparable;
-		var SortedSet = require('jsclass/src/set').SortedSet;
-		var Node = new Class( {
-			include : Comparable,
-			initialize: function( a, b, c, d ) {	
-				
-				// At least the first parameter has to be non-null
-				console.assert( a );
-
-				this.bin = a;
-				this.date = b;
-				this.counter = 0;
-				this.field = 0;
-				this.updated = true;
-			},
-			compareTo : function( other ) {
-				if(this.bin < other.bin) return -1;
-				if(this.bin > other.bin) return +1;
-				return 0;
-			},
-			equals: function( other ) {
-				return (other instanceof this.klass) && this.bin ==  other.bin;
-			}
-		} );
-
-		var _dateValuer = Number;
-		var _valuer = Number;
-		var _intervals = [ ];
-
-		var _depth = 9;
-		var _tree = [ ];
-		var _maxNodeSize = 100;
-
-		function increment( set, obj, value  ) {
-			var index = set._indexOf( obj );
-			if( index !== -1) {
-				set._members[ index ].updated = true;
-				set._members[ index ].counter++;
-				set._members[ index ].field += value;
-			}
-		}
-
-		function tree() { 
-		}
-
-		tree.updated = function( depth, date, value ) {
-			var set = _tree[ depth ];
-			var bin = Math.floor( +date / _intervals[ depth ] );
-			var tmp = new Node( bin, _intervals[ depth ] * bin );
-			var index = set._indexOf( tmp );
-
-			console.assert( index != -1);
-			set._members[ index ].updated = value;
-
-			return tree;
-		}
-
-		tree.getBin = function( depth, date ) {
-			var set = _tree[ depth ];
-			var bin = Math.floor( date / _intervals[ depth ] );
-			var tmp = new Node( bin, _intervals[ depth ] * bin );
-			var index = set._indexOf( tmp );
-			return set._members[ index ];
-		}
-
-		tree.clear = function() {
-			_intervals = [];
-			_tree = [];
-			return tree;
-		}
-
-		tree.build = function( ) {
-			if(_tree.length != 0 && _intervals.length != 0)
-				return;
-
-			var milliseconds = 1000.0;
-			_intervals = [];
-			_tree = [];
-
-			for(var i = 0; i < _depth; ++i) {
-				_tree[ i ] = new SortedSet();
-				_intervals[ i ] = milliseconds; 
-				milliseconds *= 4;
-			}
-			return tree;
-		}
-
-		tree.add = function( _ ) {
-
-			var bin, node, tmp;
-			var date = +_dateValuer.call( this, _ );
-			var value = _valuer.call( this, _ );
-
-			// Adding new data to the three
-			for( var i = 0; i < _depth; ++i ) {
-				bin = Math.floor( date / _intervals[ i ] );
-				tmp = new Node( bin, _intervals[ i ] * bin );
-				
-				_tree[ i ].add( tmp )
-				node = increment( _tree[ i ], tmp,  value );
-			}
-
-			// Removing data if the tree becomes too long
-			for( var i = 0; i < _depth; ++i )
-				while(_tree[ i ].length > _maxNodeSize) {
-					_tree[ i ].remove( new Node( _tree[ i ].first( 1 )[ 0 ].bin ) );
-				}
-
-			// Sanity check
-			for( var i = 0; i < _depth; ++i ) 
-				console.assert( _tree[ i ].length <= _maxNodeSize );
-
-			return tree;
-		}
-
-		tree.depth = function( _ ) {
-			if( arguments.length == 0 )
-				return _depth;
-			_depth = _;
-			return tree;
-		}
-
-		tree.interval = function( _ ) {
-			return _intervals[ _ ];
-		}
-
-		tree.dateValuer = function( _ ) {
-			if( arguments.length == 0 )
-				return _;
-			_dateValuer = _;
-			return tree;
-		}
-
-		tree.valuer = function( _ ) {
-			if( arguments.length == 0 )
-				return _;
-			_valuer = _;
-			return tree;
-		}
-
-		tree.get = function( _ ) {
-			console.assert( arguments.length == 1 );
-			return _tree[ _ ].select( function(d) { return true } );
-		}
-
-		return tree;
+	TimeTree : function( depth, valuer ) {
+		return new TimeTree( depth, valuer )
 	}
 }
