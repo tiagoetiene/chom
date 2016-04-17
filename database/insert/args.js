a8f40f5394a2605d583b3d21392a7062faf40275
Tiago Etiene <tiago.etiene@gmail.com>
2016-04-17 10:46:06 -0700

    Add option to echo after insert into database

--- database/insert/args.js
+++ database/insert/args.js
@@ -3,13 +3,14 @@ var fs = require( 'fs' );
 program = require( 'commander' );
 
 program
-	.version( '0.0.1' )
-	.option( '-m, --mongo [index]', 'index that represents the database (use chom database credentials list to obtain it)' )
-	.option( '-c, --collection [name]', 'name of the collection to be saved to' )
-	.option( '-s, --save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
-	.option( '-i, --simulate', 'do not save to database, only simulate', false, false )
-	.parse( process.argv );
+  .version( '0.0.1' )
+  .option( '-m, --mongo [index]', 'index that represents the database (use chom database credentials list to obtain it)' )
+  .option( '-e, --echo', 'Echo the data back to terminal', false, false )
+  .option( '-c, --collection [name]', 'name of the collection to be saved to' )
+  .option( '-s, --save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
+  .option( '-i, --simulate', 'do not save to database, only simulate', false, false )
+  .parse( process.argv );
 
 if( process.argv.length <= 2 ) {
-	program.help();
+  program.help();
 }
--- database/set/args.js
+++ database/set/args.js
@@ -3,14 +3,15 @@ var fs = require( 'fs' );
 program = require( 'commander' );
 
 program
-	.version( '0.0.1' )
-	.option( '-m, --mongo [index]', 'index that represents the database (use \'chom database credentials list\' to obtain it)' )
-	.option( '-c, --collection [name]', 'name of the collection to be saved to' )
-	.option( '-k, --key [name]', 'key to be used to identify documents' )
-	.option( '-s, --save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
-	.option( '-i, --simulate', 'do not save to database, only simulate', false, false )
-	.parse( process.argv );
+  .version( '0.0.1' )
+  .option( '-m, --mongo [index]', 'index that represents the database (use \'chom database credentials list\' to obtain it)' )
+  .option( '-c, --collection [name]', 'name of the collection to be saved to' )
+  .option( '-e, --echo', 'Echo the data back to terminal', false, false )
+  .option( '-k, --key [name]', 'key to be used to identify documents' )
+  .option( '-s, --save-interval [milliseconds]', 'timeinterval used to save bulk data', 10000, parseInt )
+  .option( '-i, --simulate', 'do not save to database, only simulate', false, false )
+  .parse( process.argv );
 
 if( process.argv.length <= 2 ) {
-	program.help();
+  program.help();
 }
